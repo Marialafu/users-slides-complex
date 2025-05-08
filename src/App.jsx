8d8d29c232bc81761a9581f091273ff4f67be310
Marialafu <lafuentelhmaria@gmail.com>
2025-05-08 11:43:12 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Button from './components/button/Button';
+
+const App = () => {
+  return (
+    <>
+      <Button />
+      <h1>React</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/button/Button.jsx
@@ -0,0 +1,5 @@
+const Button = () => {
+   return <button>CLICK ME</button>
+}
+
+export default Button
