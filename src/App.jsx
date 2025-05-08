082076d258df24850c11c04d495a88bc09002e7f
Marialafu <lafuentelhmaria@gmail.com>
2025-05-08 20:33:56 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import { useState } from "react";
-import Filters from "./components/filters/Filters";
 import UsersCard from "./components/usersCard/UsersCard";
 import {USERS} from './constants/usersInfo'
-import { v4 } from "uuid";
 
 const App = () => {
-
-  const [users, setUsers] = useState(USERS)
 
   return <>
     <header>
       <h1>Listado de usuarios</h1>
     </header>
     <main>
-      <Filters/>
-      {users.map(user => {
-        return (
-      <UsersCard key={v4()}  {...user} />)
-    })}
+      <UsersCard {...USERS} />
     </main>
   </>;
 };
--- src/components/usersCard/UsersCard.jsx
+++ src/components/usersCard/UsersCard.jsx
@@ -1,29 +1,78 @@
 import styles from './usersCard.module.css'
+import {USERS} from '../../constants/usersInfo'
+import { v4 } from "uuid";
+import { useState } from 'react';
 
-const UsersCard = ({profileImage, name, username, active}) => {
+let users = [...USERS]
 
-    const state = active ? 'Activo' : 'Inactivo'
+const UsersCard = ({active}) => {
+
+    //por qué cuando paso el active me pone todos inactivos, antes si me iba bien pero cuando lo he juntado ha dejado de funcionar.
+    const stateText = active ? 'Activo' : 'Inactivo'
     const stateColor = active ? 'green' : 'red'
+    
+    const [status, setStatus] = useState(false)
 
     return (
-        <div className={styles.cardContainer}>
+        <>
+        <div className={styles.filterContainer}>
+            <input type="text" onInput={searcherBar} />
+            <div>
+                <input type="checkbox" onChange={activeFilterButton(status, setStatus)}></input>
+                <label>Activos</label>
+            </div>
+            <select>
+                <option value="default">Por Defecto</option>
+                <option value="name">Por Nombre</option>
+            </select>
+
+        </div>
+        {users.map(user => {
+        return (
+            
+         <div key={v4()} className={styles.cardContainer}>
             <div className={styles.userInfoContainer}>
-                <img className={styles.userImage} src={profileImage} alt="" />
+                <img className={styles.userImage} src={user.profileImage} alt="" />
                 <div className={styles.userTextInfoContainer}>
-                  <h2 className={styles.text}>{name}</h2>
+                  <h2 className={styles.text}>{user.name}</h2>
                   <span>@
-                    {username}
+                    {user.username}
                   </span>
                 </div>
                 
             </div>
             <div className={styles.userState}>
-                <span className={styles[stateColor]}>{state}</span>
+                <span className={styles[stateColor]}>{stateText}</span>
                 <button>Ver Detalles</button>
             </div>
-        </div>
+         </div>
+    )
+    })}
+        </>
     )
 }
 
+const searcherBar = event => {
+    const inputText = event.target.value
+    const inputTextLenght = inputText.length
+
+    users =  USERS.filter(user => {
+     return  user.name.charAt(inputTextLenght - 1) === inputText.charAt(inputTextLenght - 1)
+    })
+
+}
+
+const activeFilterButton = (status, setStatus) => {
+    setStatus = !status
+
+    if (status){
+    users =  USERS.filter(user => {
+     return  user.active === true
+    })
+    } else {
+        users = [...USERS]
+    }
+
+}
 
 export default UsersCard
